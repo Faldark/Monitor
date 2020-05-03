44a8bea12d793573a3e0f8a2388b94522dabbff7
Bogdan Chukhno <Bogdan.Chukhno@gmail.com>
2020-05-03 10:07:38 +0300

    implemented page autorefresh in 2 min interval

--- Web/ClientApp/src/app/monitor/monitor.component.ts
+++ Web/ClientApp/src/app/monitor/monitor.component.ts
@@ -1,6 +1,8 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { timer, Observable, Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 interface Monitor {
   url: string;
@@ -15,29 +17,35 @@ interface Monitor {
   selector: 'app-monitor-component',
   templateUrl: './monitor.component.html'
 })
-export class MonitorComponent {
+export class MonitorComponent implements OnInit {
   public monitors: Monitor[];
   private url: string;
+  subscription: Subscription;
+  constructor(private readonly http: HttpClient, @Inject('BASE_URL') baseUrl: string, private readonly router: Router) {
+    this.url = baseUrl;
+  }
+  ngOnInit(): void {
 
-  constructor(private readonly http: HttpClient,  @Inject('BASE_URL')  baseUrl: string, private readonly router: Router) {
-    http.get<Monitor[]>(baseUrl + 'monitor').subscribe(result => {
-      this.monitors = result;
-    }, error => console.error(error));
+    this.subscription = timer(0, 120000).pipe(
+      switchMap(() => this.getMonitors())
+    ).subscribe(monitors => this.monitors = monitors);
   }
 
   addUrl(): void {
 
     this.router.navigate(['monitor/addUrl']);
   }
+
   removeUrl(): void {
 
     const urls = new Array();
     this.monitors.forEach(monitor => {
       urls.push(monitor.url);
     });
-    this.router.navigate(['monitor/removeUrl'], {state: {monitors: urls}});
+    this.router.navigate(['monitor/removeUrl'], { state: { monitors: urls } });
+  }
+
+  getMonitors(): Observable<Monitor[]> {
+    return this.http.get<Monitor[]>(this.url + 'monitor');
   }
 }
-
-
-
